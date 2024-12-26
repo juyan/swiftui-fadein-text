c583a0a1d825deb2015326b2e37b8fded159ee9e
Jun Yan <junyan.cpp@gmail.com>
2024-12-26 00:18:19 -0800

    flatten dir

--- Sources/FadeInText/FadeInText.swift
+++ Sources/FadeInText/FadeInText.swift
@@ -24,36 +24,50 @@ public struct FadeInText: View {
   }
 }
 
+#if DEBUG
+
+struct ControlledView: View {
+  
+  let text: String
+  let config: LinearInterpolatorConfig
+  @State var show = false
+  
+  var body: some View {
+    VStack {
+      if show {
+        FadeInText(text: text, color: .black, tokenizer: DefaultTokenizer(), interpolator: LinearInterpolator(config: config))
+          .font(.title2)
+      }
+      Spacer()
+    }
+    .padding(16)
+    .onAppear(perform: {
+      Task {
+        try await Task.sleep(nanoseconds: 5000000000)
+        self.show = true
+      }
+    })
+  }
+}
 
 #Preview("English") {
-  FadeInText(
+  ControlledView(
     text: "Hello! Welcome to FadeInText. The text will fade in smoothly. You are able to tweak the fade in duration by passing a configuration. Enjoy the animation.",
-    color: .black,
-    tokenizer: DefaultTokenizer(),
-    interpolator: LinearInterpolator(config: .defaultValue)
+    config: .defaultValue
   )
-  .font(.title2)
-  .padding(16)
 }
 
 #Preview("English-Slow") {
-  FadeInText(
+  ControlledView(
     text: "Hello! Welcome to FadeInText. The text will fade in smoothly. You are able to tweak the fade in duration by passing a configuration. Enjoy the animation.",
-    color: .black,
-    tokenizer: DefaultTokenizer(),
-    interpolator: LinearInterpolator(config: .init(fadeInDuration: 2.0, appearanceDuration: 5.0))
+    config: .init(fadeInDuration: 2.0, appearanceDuration: 5.0)
   )
-  .font(.title2)
-  .padding(16)
 }
 
 #Preview("Chinese") {
-  FadeInText(
+  ControlledView(
     text: "壬戌之秋，七月既望，苏子与客泛舟游于赤壁之下。清风徐来，水波不兴。举酒属客，诵明月之诗，歌窈窕之章。少焉，月出于东山之上，徘徊于斗牛之间。白露横江，水光接天。纵一苇之所如，凌万顷之茫然。浩浩乎如冯虚御风，而不知其所止；飘飘乎如遗世独立，羽化而登仙。",
-    color: .black,
-    tokenizer: DefaultTokenizer(),
-    interpolator: LinearInterpolator(config: .defaultValue)
+    config: .init(fadeInDuration: 2.0, appearanceDuration: 5.0)
   )
-  .font(.title2)
-  .padding(16)
 }
+#endif
